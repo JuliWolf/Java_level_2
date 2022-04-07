3f9d686eaef077614faeace023a320f589ec51ac
Julia Ivanova <for.julia96@gmail.com>
2022-04-07 17:59:00 +0300

    Homework to lesson 6

--- src/main/java/Lesson_6/Homework/EchoServer.java
+++ src/main/java/Lesson_6/Homework/EchoServer.java
@@ -5,28 +5,114 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class EchoServer {
+public class EchoServer implements IIsEndMessage{
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    private Thread listenThread;
+    private Thread scannerThread;
 
     public static void main(String[] args) {
-        try (ServerSocket serverSoket = new ServerSocket(8189)) {
-            System.out.println("Сервер запущен, ожидаем подключения...");
-            Socket socket = serverSoket.accept();
-            System.out.println("Клиент подключился");
-            final DataInputStream in = new DataInputStream(socket.getInputStream());
-            final DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        EchoServer server = new EchoServer();
+        server.start();
+    }
 
-            while (true) {
-                final String message = in.readUTF();
-                if ("/end".equalsIgnoreCase(message)) {
-                    out.writeUTF(message);
-                    break;
-                }
-                out.writeUTF("echo: " + message);
-            }
-        }catch (IOException err) {
+    private void start () {
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            System.out.println("Сервер запущен, ожидаем подключения...");
+            socket = serverSocket.accept();
+            System.out.println("Клиент подключился");
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+
+            startScanner();
+            startListener();
+
+            listenThread.join();
+
+        } catch (IOException err) {
             err.printStackTrace();
             throw new RuntimeException(err);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            closeConnection();
         }
+    }
+
+    private void closeConnection() {
+        if (in != null) {
+            try {
+                in.close();
+            } catch (IOException err) {
+                err.printStackTrace();
+            }
+        }
+
+        if (out != null) {
+            try {
+                out.close();
+            } catch (IOException err) {
+                err.printStackTrace();
+            }
+        }
+
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException err) {
+                err.printStackTrace();
+            }
+        }
+    }
+
+    private void startListener () {
+        listenThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        final String message = in.readUTF();
+
+                        if (isEndMessage(message)) {
+                            out.writeUTF(message);
+                            break;
+                        }
+                        out.writeUTF("echo: " + message);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        listenThread.start();
+    }
+
+    private void startScanner () throws IOException {
+        final Scanner scanner = new Scanner(System.in);
+        scannerThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                final String message = scanner.nextLine();
+                try {
+                    if (isEndMessage(message)) {
+                        out.writeUTF(message);
+                        return;
+                    }
+
+                    out.writeUTF("New: " + message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        scannerThread.start();
     }
 }
